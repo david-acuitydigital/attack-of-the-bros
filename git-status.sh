0cbc6c79fe56927d1aa19d1a0fe963ea7bbf8f74
david-acuitygitigal <david@acuitydigital.com>
2019-05-02 13:29:32 -0500

    update git-status.sh

--- git-status.sh
+++ git-status.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-state=$1
-commit=$2
-GITHUB_URL=https://api.github.com/repos//david-acuitydigital/attack-of-the-bros/statuses
+STATE=$1
+COMMIT=$2
+CONTEXT="Unit Tests - Jenkins"
+GITHUB_URL=https://api.github.com/repos/david-acuitydigital/attack-of-the-bros/statuses
 BUILD_URL="https://jenkins.ctnr.ctl.io"
-if [[ -z "${state}" ]] || [[ -z "${commit}" ]] || [[ -z "${REPO_USER}" ]] || [[ -z "${REPO_PASSWORD}" ]] ; then
+if [[ -z "${STATE}" ]] || [[ -z "${COMMIT}" ]] || [[ -z "${REPO_USER}" ]] || [[ -z "${REPO_PASSWORD}" ]] ; then
   echo "Usage:"
   echo "git-status.sh state commit"
   echo '  state - state of the status vaild values are "success", or "failure"'
@@ -11,6 +12,5 @@ if [[ -z "${state}" ]] || [[ -z "${commit}" ]] || [[ -z "${REPO_USER}" ]] || [[ 
   echo '** NOTE REPO_USER and REPO_PASSWORD must be exported environment variables for a user with access to ${GITHUB_URL}'
   exit 1
 fi
-curl -s -u ${REPO_USER}:${REPO_PASSWORD} -X POST --header "Content-Type: application/json" \
-     --data "{\"state\": \"${state}\", \"target_url\": \"${BUILD_URL}\", \"context\": \"${context}\"}" \
-     ${GITHUB_URL}/${commit}
+
+curl -s -u ${REPO_USER}:${REPO_PASSWORD} -X POST --header "Content-Type: application/json" --data "{\"state\": \"${STATE}\", \"target_url\": \"${BUILD_URL}\", \"context\": \"${CONTEXT}\"}" ${GITHUB_URL}/${COMMIT}
